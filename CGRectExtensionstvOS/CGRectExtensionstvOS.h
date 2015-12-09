9892af24899d8a91dd6d2c54799cf7bdfaf44da4
toshi0383 <t.suzuki326@gmail.com>
2015-12-09 12:33:59 +0900

    add tvOS target supporting tvOS9.0 and greater

---
+++ CGRectExtensionstvOS/CGRectExtensionstvOS.h
@@ -0,0 +1,19 @@
+//
+//  CGRectExtensionstvOS.h
+//  CGRectExtensionstvOS
+//
+//  Created by toshi0383 on 12/9/15.
+//  Copyright © 2015 Nikolaj Schumacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CGRectExtensionstvOS.
+FOUNDATION_EXPORT double CGRectExtensionstvOSVersionNumber;
+
+//! Project version string for CGRectExtensionstvOS.
+FOUNDATION_EXPORT const unsigned char CGRectExtensionstvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGRectExtensionstvOS/PublicHeader.h>
+
+
